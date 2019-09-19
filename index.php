b74877e0fa93f2185d5c43fd5fa40e7ff5d5cad8
yunggenius <yunggenius4@gmail.com>
2019-09-19 10:05:57 +0100

    added login authentication. tweaked login page UI.

--- index.php
+++ index.php
@@ -1,61 +1,26 @@
+<?php session_start(); /* Starts the session */
+
+if(!isset($_SESSION['UserData']['Username'])){
+    header("location:login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>login page</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" type="text/css" href="css/main.css">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
- <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap" rel="stylesheet">
+	<title>Welcome!</title>
+	 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<!-- <h1 class="center">The Team Of The Creators</h1> -->
-  
 
 
-<div class="row">
-<div class= "card column to-left">
-	<h2 class="center">Welcome back</h2>	
-	<form>
-		 <label>
-                    <input type="text" name="username" placeholder="Username or email" >
-                </label>
-            <br><br><br>
-            <label>
-                <input type="password" name="password" placeholder="Password" >
-            </label>
-            <br><br><br>
-           
-            <input type="submit" class="btn" value="Login"><br><br>
-             <div class="afterform">
-            	<input type="checkbox"><a href="#" class="anchor">Remember me</a> <a class="anchor" href="#">Forgot password?</a>
-            </div><br>
-	</form>
-	<br><br>
-	<!--<hr class="x-axis"><span class="dot"></span></hr>-->
+<center>
+Congratulation! It works! 
 <br>
-<div class="socio">
-<a href="#" class="fa fa-facebook"></a>
-<a  href="#" class="fa fa-slack" ></a>
-<a href="#" class="fa fa-twitter"></a>
+<a href="logout.php" class="btn2">Click here</a> 
+<br>
+to Logout.
 
-<p>Don't have an account?<a href="#" class="anchor1">Sign up here</a></p>
-</div>
-</div>
-
-
-
-	<img class="img" src="img/major2.jpg">
-
-</div>
-<div>
-	<img class="bottom-img" src="img/crid_desktopnew.png"></img>
-</div>
-
+</center>
 </body>
-
-
 </html>
-
-<?php
-echo "hello world";
-?>
